2c16c6ac7f252bcc59514674e6cb21be824d5b9b
Sean Blessing <snblessing@gmail.com>
2025-04-02 16:00:20 -0400

    if, for, hidden, click, bootstrap demos

---
+++ src/app/list/list.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list',
+  standalone: false,
+  templateUrl: './list.component.html',
+  styleUrl: './list.component.css'
+})
+export class ListComponent {
+  title: string = "List Component";
+  displayable: boolean = false;
+  fruits: string[] = ["apple", "orange", "kiwi"];
+  newFruit: string = "";
+
+  addFruit() {
+    this.fruits.push(this.newFruit);
+  }
+}
